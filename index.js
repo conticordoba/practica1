f5afb29c5b639e7c7eaded7aefae0fa2af2e402b
conticordoba <cordobasantillan.maria@iesconcepcion.edu.ar>
2025-06-07 18:24:49 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("buenas")
+console.log("Bienvenidos")
+
+//numero mayor y menor
+let numero2 = parseInt(prompt("ingese un numero:"));
+if (numero2 > 0) {
+    console.log("el numero es postivo");
+} else if (numero2 === 0) {
+    console.log("el numero es cero");
+} else {
+    console.log("el numero es negativo");
+}
